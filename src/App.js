19bd81841c1be3ba54d0588aa20cc8b74f38ad4f
CodeSandbox <support@codesandbox.io>
2023-08-17 14:24:01 +0000

    Sandbox snapshot commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import JokeList from "./JokeList";
+
+/** App component. Renders list of jokes. */
+
+export default function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
+}
